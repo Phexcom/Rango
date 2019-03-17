c9cd55117525cb8b59cc70a1bfc4c60199d673bc
Phexcom <phexcom022@gmail.com>
2019-03-17 11:59:31 -0400

    Added a script to populate data to the database instance

--- populate_rango.py
+++ populate_rango.py
@@ -1,6 +1,7 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tango_with_django_project.settings')
+
 import django
 django.setup()
 from rango.models import Category, Page
