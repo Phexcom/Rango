66526a7f5034f18ce0c0a28f5875543ad7d3d22b
Phexcom <phexcom022@gmail.com>
2019-03-16 22:47:40 -0400

    Setting up Django Templates and Serving Static and Media Files

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango Say hey dear partner!")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    return render(request, 'rango/about.html')
+
